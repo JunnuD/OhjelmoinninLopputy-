c75713c040a085fc32f15289bcdff16dac864b9b
Junnu Danhammer <junnudanhammer@gmail.com>
2023-04-11 20:05:24 +0300

    Update Tietovisapeli.java

--- Tietovisapeli.java
+++ Tietovisapeli.java
@@ -7,7 +7,7 @@ import java.io.IOException;
     
     public class Tietovisapeli {
     //katothan itha
-        public static void main(String[] args) throws FileNotFoundException {
+        public static void (String[] args) throws FileNotFoundException {
             Scanner lukija = new Scanner(System.in);
             System.out.println("Nimeä tiedostosi: (.txt pääte tulee automaattisesti.) ");
             File tiedostoNimi = new File(lukija.nextLine() + ".txt");
@@ -38,8 +38,54 @@ import java.io.IOException;
                 System.out.println("Kirjoitus epäonnistui nimeämääsi tiedostoon. :( ");
             }
         }
-    }
     
+        /**
+         * TÄSTÄ ALKAA ITSE PELI JA TÄTÄ ENNEN TEHDÄÄN SE TIEDOSTON LUONTI PROSESSI
+         * 
+         * 
+         * 
+         */
+        public static void main(String[] args) {
+        String[] maat = new String[5];
+        String[] pääkaupungit = new String[5];
+        Scanner lukija = new Scanner(System.in);
+
+        try {
+            File tiedosto = new File("maat.txt");
+            Scanner tiedostonLukija = new Scanner(tiedosto);
+
+            int indeksi = 0;
+            while (tiedostonLukija.hasNextLine() && indeksi < 5) {
+                String rivi = tiedostonLukija.nextLine();
+                String[] tiedot = rivi.split(":");
+                maat[indeksi] = tiedot[0];
+                pääkaupungit[indeksi] = tiedot[1];
+                indeksi++;
+            }
+            tiedostonLukija.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Tiedostoa ei löydy.");
+            return;
+
+        }
+
+        int oikein = 0;
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Mikä on " + maat[i] + "n" + " pääkaupunki?");
+            String vastaus = lukija.nextLine();
+            if (vastaus.equalsIgnoreCase(pääkaupungit[i])) {
+                System.out.println("Oikein!");
+                oikein++;
+            } else {
+                System.out.println("Väärin, oikea vastaus on " + pääkaupungit[i]);
+            }
+        }
+
+        System.out.println("Peli päättyi. Saavutit " + oikein + " pistettä.");
+          
+    } 
+}
+
     
     
     // Useampi metodi.
