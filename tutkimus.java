1dfff9ed8fca550de42879bc88ebc8820e646dbd
alist3rz <alister_gul@hotmail.com>
2023-04-11 20:50:08 +0300

    Update tutkimus.java

--- tutkimus.java
+++ tutkimus.java
@@ -1,69 +1,65 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Ohjelma {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
+        
+        // Kysytään käyttäjältä tiedoston nimi ja luodaan uusi tiedosto
         Scanner lukija = new Scanner(System.in);
-        System.out.println("Nimeä tiedostosi: (.txt pääte tulee automaattisesti.) ");
-        File tiedostoNimi = new File(lukija.nextLine() + ".txt");
-
-        System.out.println("Syötä tiedostoon sisältöä, vaikka yksi lause. Kirjoita: ");
-        String sisältö = lukija.nextLine();
-
-        kirjoitaTiedostoon(tiedostoNimi, sisältö);
-
-        String[] maat = new String[5];
-        String[] pääkaupungit = new String[5];
-
+        System.out.println("Anna tiedoston nimi: ");
+        String tiedostoNimi = lukija.nextLine();
+        tiedostoNimi += ".txt";
         try {
-            File tiedosto = new File("maat.txt");
-            Scanner tiedostonLukija = new Scanner(tiedosto);
-
-            int indeksi = 0;
-            while (tiedostonLukija.hasNextLine() && indeksi < 5) {
-                String rivi = tiedostonLukija.nextLine();
-                String[] tiedot = rivi.split(":");
-                maat[indeksi] = tiedot[0];
-                pääkaupungit[indeksi] = tiedot[1];
-                indeksi++;
+            File tiedosto = new File(tiedostoNimi);
+            tiedosto.createNewFile();
+            System.out.println("Tiedosto " + tiedostoNimi + " luotu.");
+        } catch (IOException e) {
+            System.out.println("Tiedoston luonti epäonnistui.");
+        }
+        
+        // Kysytään käyttäjältä pokemonien nimiä ja tallennetaan ne tiedostoon
+        try {
+            PrintWriter kirjoittaja = new PrintWriter(new FileWriter(tiedostoNimi, true));
+            String vastaus;
+            do {
+                System.out.println("Anna pokemonin nimi: ");
+                String nimi = lukija.nextLine();
+                kirjoittaja.println(nimi);
+                System.out.println("Jatketaanko? (kyllä/ei)");
+                vastaus = lukija.nextLine();
+            } while (vastaus.equals("kyllä"));
+            kirjoittaja.close();
+            System.out.println("Pokemonit tallennettu tiedostoon.");
+        } catch (IOException e) {
+            System.out.println("Tiedostoon kirjoittaminen epäonnistui.");
+        }
+        
+        // Luetaan tiedosto ja tallennetaan pokemonit listaan
+        ArrayList<String> pokemonit = new ArrayList<>();
+        try {
+            Scanner tiedostonLukija = new Scanner(new File(tiedostoNimi));
+            while (tiedostonLukija.hasNextLine()) {
+                pokemonit.add(tiedostonLukija.nextLine());
             }
             tiedostonLukija.close();
         } catch (FileNotFoundException e) {
             System.out.println("Tiedostoa ei löydy.");
-            return;
-
         }
-
-        int oikein = 0;
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Mikä on " + maat[i] + "n" + " pääkaupunki?");
+        
+        // Aloita muistipeli
+        int pisteet = 0;
+        for (String pokemon : pokemonit) {
+            System.out.println("Mikä oli " + pokemon + "?");
             String vastaus = lukija.nextLine();
-            if (vastaus.equalsIgnoreCase(pääkaupungit[i])) {
+            if (vastaus.equals(pokemon)) {
                 System.out.println("Oikein!");
-                oikein++;
+                pisteet++;
             } else {
-                System.out.println("Väärin, oikea vastaus on " + pääkaupungit[i]);
+                System.out.println("Väärin. Oikea vastaus oli " + pokemon + ".");
             }
         }
-
-        System.out.println("Peli päättyi. Saavutit " + oikein + " pistettä.");
-
-    }
-
-    public static void kirjoitaTiedostoon(File tiedostoNimi, String sisältö) {
-        try {
-            FileWriter kirjoittaja = new FileWriter(tiedostoNimi);
-            kirjoittaja.write(sisältö);
-            kirjoittaja.close();
-            System.out.println("Kirjoitus onnistui nimeämääsi tiedostoon! ");
-
-        } catch (IOException e) {
-            System.out.println("Kirjoitus epäonnistui nimeämääsi tiedostoon. :( ");
-        }
+        
+        System.out.println("Peli päättyi. Saavutit " + pisteet + " pistettä. Kiitos pelistä!");
     }
 }
