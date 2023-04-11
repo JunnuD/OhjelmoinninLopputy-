343d5661eab6dd2b627ef12589b3ddb795ef59f7
alist3rz <alister_gul@hotmail.com>
2023-04-11 21:02:19 +0300

    Update tutkimus.java

--- tutkimus.java
+++ tutkimus.java
@@ -2,64 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Ohjelma {
-
     public static void main(String[] args) {
-        
-        // Kysytään käyttäjältä tiedoston nimi ja luodaan uusi tiedosto
-        Scanner lukija = new Scanner(System.in);
-        System.out.println("Anna tiedoston nimi: ");
-        String tiedostoNimi = lukija.nextLine();
-        tiedostoNimi += ".txt";
+        Scanner input = new Scanner(System.in);
+        ArrayList<String> pokemonit = new ArrayList<String>();
+        ArrayList<String> tyypit = new ArrayList<String>();
+
+        // Luo uusi tiedosto
+        System.out.print("Anna tiedoston nimi: ");
+        String tiedostoNimi = input.nextLine();
         try {
             File tiedosto = new File(tiedostoNimi);
             tiedosto.createNewFile();
-            System.out.println("Tiedosto " + tiedostoNimi + " luotu.");
         } catch (IOException e) {
-            System.out.println("Tiedoston luonti epäonnistui.");
+            System.out.println("Tiedoston luominen epäonnistui.");
+            return;
         }
-        
-        // Kysytään käyttäjältä pokemonien nimiä ja tallennetaan ne tiedostoon
-        try {
-            PrintWriter kirjoittaja = new PrintWriter(new FileWriter(tiedostoNimi, true));
-            String vastaus;
-            do {
-                System.out.println("Anna pokemonin nimi: ");
-                String nimi = lukija.nextLine();
-                kirjoittaja.println(nimi);
-                System.out.println("Jatketaanko? (kyllä/ei)");
-                vastaus = lukija.nextLine();
-            } while (vastaus.equals("kyllä"));
-            kirjoittaja.close();
-            System.out.println("Pokemonit tallennettu tiedostoon.");
-        } catch (IOException e) {
-            System.out.println("Tiedostoon kirjoittaminen epäonnistui.");
-        }
-        
-        // Luetaan tiedosto ja tallennetaan pokemonit listaan
-        ArrayList<String> pokemonit = new ArrayList<>();
-        try {
-            Scanner tiedostonLukija = new Scanner(new File(tiedostoNimi));
-            while (tiedostonLukija.hasNextLine()) {
-                pokemonit.add(tiedostonLukija.nextLine());
+
+        // Syötä Pokemonin nimi ja tyyppi tiedostoon
+        boolean jatka = true;
+        while (jatka) {
+            System.out.print("Anna Pokemonin nimi: ");
+            String nimi = input.nextLine();
+            pokemonit.add(nimi);
+
+            System.out.print("Anna Pokemonin tyyppi: ");
+            String tyyppi = input.nextLine();
+            tyypit.add(tyyppi);
+
+            System.out.print("Haluatko jatkaa? (kyllä/ei) ");
+            String vastaus = input.nextLine();
+            if (!vastaus.equalsIgnoreCase("kyllä")) {
+                jatka = false;
             }
-            tiedostonLukija.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Tiedostoa ei löydy.");
         }
-        
-        // Aloita muistipeli
+
+        // Pelaa muistipeliä
         int pisteet = 0;
-        for (String pokemon : pokemonit) {
-            System.out.println("Mikä oli " + pokemon + "?");
-            String vastaus = lukija.nextLine();
-            if (vastaus.equals(pokemon)) {
+        for (int i = 0; i < pokemonit.size(); i++) {
+            String pokemon = pokemonit.get(i);
+            String tyyppi = tyypit.get(i);
+
+            System.out.print("Mikä on " + pokemon + " tyyppi? ");
+            String vastaus = input.nextLine();
+            if (vastaus.equalsIgnoreCase(tyyppi)) {
                 System.out.println("Oikein!");
                 pisteet++;
             } else {
-                System.out.println("Väärin. Oikea vastaus oli " + pokemon + ".");
+                System.out.println("Väärin, oikea vastaus oli " + tyyppi + ".");
             }
         }
-        
-        System.out.println("Peli päättyi. Saavutit " + pisteet + " pistettä. Kiitos pelistä!");
+
+        // Tulosta pisteet ja kiitos
+        System.out.println("Sait " + pisteet + " pistettä!");
+        System.out.println("Kiitos pelaamisesta!");
     }
 }
